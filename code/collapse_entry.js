c9cc9a1dc569e13c7c70e962c8b47df71951706e
Ultrad00d <132278802+Ultrad00d@users.noreply.github.com>
2025-01-18 00:02:47 +0300

    hotfix

--- code/collapse_entry.js
+++ code/collapse_entry.js
@@ -8,20 +8,9 @@ for (i = 0; i < coll.length; i++) {
     content.classList.toggle("collapsible-open");
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-
-      // content.style.width = null;
-      // content.style.transform = null;
-      // content.style.padding = null;
-
       this.style.width = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-
-      // content.style.width = "calc(100%)";
-      // content.style.transform = "translateX(calc(-2vw - 2px))";
-      // content.style.padding = "0 2vw";
-
-      // this.style.width = "100%";
     } 
   });
 }
