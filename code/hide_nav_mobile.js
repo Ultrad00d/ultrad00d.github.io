9fe36c1c439fd84634214e681319ab6ccfba327f
Ultrad00d <132278802+Ultrad00d@users.noreply.github.com>
2025-02-04 23:02:41 +0300

    Исправление нежеланного поведения
    При перезагрузке страницы навигационная панель теперь правильно исчезает.

--- code/hide_nav_mobile.js
+++ code/hide_nav_mobile.js
@@ -1,10 +1,14 @@
+function resetCheckbox() {
+    document.getElementById('table-of-contents-active').checked = false;
+}
+
 function isSmallScreen() {
     return window.matchMedia("(max-width: 1100px)").matches;
 }
 
 function handleClick() {
     if (isSmallScreen()) {
-        document.getElementById('table-of-contents-active').checked = false;
+        resetCheckbox();
     }
 }
 
@@ -13,3 +17,4 @@ const menuItems = document.querySelectorAll('menu li');
 menuItems.forEach(item => {
     item.addEventListener('click', handleClick);
 });
+document.addEventListener('DOMContentLoaded', resetCheckbox);
